207e31a713255432e43a74db5d3b8520abef4e9d
briankipchirchir <kbrianngetich@gmail.com>
2024-06-29 11:44:29 +0300

    function-continued-lab

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ function saturdayFun(activity = "roller-skate") {
   
   
   console.log(saturdayFun());
+
+
+  //lab2//
+  const mondayWork = function(activity = "go to the office") {
+
+    return `This Monday, I will ${activity}.`;
+  };
+  
+
+  console.log(mondayWork());
+  
+  //lab3//
+
+function saturdayFun(activity = "roller-skate") {
+
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction());
